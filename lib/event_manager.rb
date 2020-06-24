82377f510e1b91c1c6662fcc7d428ad08a80deb7
Eduardo Parra <helloeduardo@icloud.com>
2020-06-23 21:12:20 -0600

    Add save_thank_you_letter method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -22,6 +22,16 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 #basic reading of file and printing names
@@ -42,18 +52,10 @@ erb_template = ERB.new template_letter
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  Dir.mkdir("output") unless Dir.exists?("output")
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
+  save_thank_you_letter(id, form_letter)
 end
